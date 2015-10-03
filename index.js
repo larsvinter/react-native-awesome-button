6bb33bf1ae14d386599d4da6a5c70ae3401b0c75
Lars Vinter <larsvinter@gmail.com>
2015-10-03 18:31:35 +0200

    Skeleton

---
+++ index.js
@@ -0,0 +1,21 @@
+var React = require('react-native')
+
+
+var {
+  Component,
+  View,
+  Text
+} = React
+
+
+class AwesomeButton extends Component {
+
+  render() {
+    return(
+      <View><Text>Lars</Text></View>
+    )
+  }
+
+}
+
+module.exports = AwesomeButton
