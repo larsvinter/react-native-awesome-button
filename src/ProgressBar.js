81590b1789859278d4c95bd7566270355bfe73c0
Madhusudhan Srinivasa <madhums8@gmail.com>
2016-07-05 23:49:47 +0200

    Use ActivityIndicator instead of ProgressBarAndroid
    ProgressBarAndroid is deprecated in latest RN. Using it throws the following warning. 
    
    Circular indeterminate `ProgressBarAndroid`is deprecated. Use `ActivityIndicator` instead.

--- src/ProgressBar.js
+++ src/ProgressBar.js
@@ -1,21 +1,13 @@
 import React, { Component } from 'react';
 
 import {
-  Platform,
-  ActivityIndicator,
-  ProgressBarAndroid
+  ActivityIndicator
 } from  'react-native';
 
 export default class ProgressBar extends Component {
   render() {
-    if (Platform.OS === 'ios') {
-      return (
-        <ActivityIndicator color={this.props.color} style={this.props.style}/>
-      );
-    } else {
-      return (
-        <ProgressBarAndroid color={this.props.color} style={this.props.style} styleAttr={"Small"}/>
-      );
-    }
+    return (
+      <ActivityIndicator color={this.props.color} style={this.props.style}/>
+    );
   }
 }
