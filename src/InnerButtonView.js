9802571dca3f967e5b68e6d3475950ffe7d52a84
Lars Vinter <larsvinter@gmail.com>
2017-02-22 13:51:07 +0100

    Refactor

--- src/InnerButtonView.js
+++ src/InnerButtonView.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { ActivityIndicator, View } from 'react-native';
+import { ActivityIndicator, View, StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
   insideView: {
