311b11a8b30dd99a1825266315c156993307b181
Lars Vinter <larsvinter@gmail.com>
2017-02-22 20:21:38 +0100

    1.7.0 refactor

--- src/OuterButtonView.js
+++ src/OuterButtonView.js
@@ -4,13 +4,9 @@ import { View, Text } from 'react-native';
 import InnerButtonView from './InnerButtonView';
 
 const OuterButtonView = (props) => {
-  const lars = 'er';
-
-  alert(JSON.stringify(props.viewState));
-
   return (
-    <View>
-      <Text>Dette {lars} en test</Text>
+    <View style={props.viewState.backgroundStyle}>
+      <Text style={props.viewState.labelStyle}>{props.viewState.text}</Text>
     </View>
   );
 };
@@ -23,7 +19,11 @@ OuterButtonView.defaultProps = {
   viewState: {
     backgroundStyle: {
       backgrouldColor: 'rgb(0, 255, 0)'
-    }
+    },
+    labelStyle: {
+      color: 'rgb(255, 255, 0)'
+    },
+    text: 'Click here'
   }
 };
 
